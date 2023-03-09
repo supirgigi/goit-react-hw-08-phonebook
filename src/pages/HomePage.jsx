92aa9523e79eb04b5023a69f51a1556494ac4fe3
supirgigi <107944673+supirgigi@users.noreply.github.com>
2023-03-09 17:54:10 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
       <Typography variant="h2" sx={{ fontSize: '2.5rem', textAlign: 'center' }}>
         Welcome to Phonebook!
       </Typography>
-      <Typography sx={{ fontSize: '1.25rem' }}>
+      <Typography sx={{ fontSize: '1.25rem', textAlign: 'center' }}>
         Sign up or log in to start using.
       </Typography>
     </Box>
